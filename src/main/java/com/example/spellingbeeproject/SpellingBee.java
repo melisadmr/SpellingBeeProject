e9d806cdc6613a8138cce61154bd212b72ef3309
mhmtgks <goksumehmetgs@gmail.com>
2022-05-25 20:43:43 +0300

    d

--- src/main/java/com/example/spellingbeeproject/SpellingBee.java
+++ src/main/java/com/example/spellingbeeproject/SpellingBee.java
@@ -11,7 +11,7 @@ public class SpellingBee extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(SpellingBee.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 750, 550);
         stage.setTitle("Spelling Bee!");
         stage.setScene(scene);
         stage.show();
